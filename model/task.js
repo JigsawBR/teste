85e56c6e83e4ca14880e9c7b7135f2cf7493e2a7
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-04-15 15:22:51 -0300

    Criando model Task

---
+++ model/task.js
@@ -0,0 +1,31 @@
+import sequelize from "../database/sequelize.js";
+import { DataTypes } from "sequelize";
+
+const Task = sequelize.define(
+    'Task',
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+      },
+      titulo: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      descricao: {
+        type: DataTypes.STRING
+      },
+      dataHora: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+      }
+    },
+    {
+      // Other model options go here
+    },
+  );
+
+Task.sync();
+
+export default Task;
