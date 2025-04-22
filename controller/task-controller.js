9b227ec27ff7bfb815fa4a967c297163c7bf9802
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-04-22 16:25:05 -0300

    Criando método para atualizar

--- controller/task-controller.js
+++ controller/task-controller.js
@@ -24,6 +24,22 @@ export async function deleteTask(req,res){
     res.status(200).json(task);
 }
 
+export async function updateTask(req,res){
+    const id = req.params.id;
+    const task = await Task.findByPk(id);
+    if(!task){
+        return res.status(404).json("Tarefa não encontrada");
+    }
+    try{
+        task.set(req.body);
+        await task.save();
+        res.status(200).json(task);
+    }catch(err){
+        console.log(err);
+        res.status(400).json("Falha ao atualizar");
+    }
+}
+
 export async function createTask(req,res){
     try{
       const newTask = await Task.create(req.body);
--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const app = express();
 app.use(express.json());
 const port = 3000;
 
-import { getTasks, createTask, findTask, deleteTask } from './controller/task-controller.js';
+import { getTasks, createTask, findTask, deleteTask, updateTask } from './controller/task-controller.js';
 
 app.get('/tasks', getTasks);
 app.get('/tasks/:id',findTask);
 app.delete('/tasks/:id', deleteTask);
 app.post('/tasks', createTask);
+app.patch('/tasks/:id', updateTask);
 
 app.listen(port, () => {
   console.log(`App de exemplo esta rodando na porta ${port}`)
