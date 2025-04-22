35f4dc91b135333ca041e2a9bffeec759b998ebb
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-04-22 16:39:05 -0300

    Utilizando router

--- index.js
+++ index.js
@@ -3,13 +3,9 @@ const app = express();
 app.use(express.json());
 const port = 3000;
 
-import { getTasks, createTask, findTask, deleteTask, updateTask } from './controller/task-controller.js';
+import TaskRouter from './router/task-router.js';
 
-app.get('/tasks', getTasks);
-app.get('/tasks/:id',findTask);
-app.delete('/tasks/:id', deleteTask);
-app.post('/tasks', createTask);
-app.patch('/tasks/:id', updateTask);
+app.use('/tasks', TaskRouter);
 
 app.listen(port, () => {
   console.log(`App de exemplo esta rodando na porta ${port}`)
