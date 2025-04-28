b3af0734c13d2560bfb5b728b6216be1d2e8ed2a
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-04-28 16:39:27 -0300

    Adicionando rota de post para usuários

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ import TaskRouter from './router/task-router.js';
 
 app.use('/tasks', TaskRouter);
 
+//TODO: Refatorar
+import User from './model/user.js';
+app.post('/users', async (req,res)=>{
+    try{
+        const user = await User.create(req.body);
+        res.status(201).json(user);
+    }catch(err){
+        console.log(err);
+        res.status(400).json("Erro ao criar usuario");
+    }
+});
+
 app.listen(port, () => {
   console.log(`App de exemplo esta rodando na porta ${port}`)
 });
